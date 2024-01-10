a4855980524afa3d7a41211ad163d8eab0ebd0bd
Doherty Cho <jdy0120120@gmail.com>
2024-01-10 11:02:42 +0900

    feat: update global style

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    main: "cyan",
+  },
+  fonts: ["sans-serif", "Roboto"],
+  fontSizes: {
+    small: "1em",
+    medium: "2em",
+    large: "3em",
+  },
+};
+
+export default theme;
